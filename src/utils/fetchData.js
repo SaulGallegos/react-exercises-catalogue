e1439d21157365df06769e0cdd04536d405dad28
Saul Gallegos <saulladrondeguevara@gmail.com>
2022-06-27 15:52:22 -0500

    key deleted

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '9304435731msh3f1cc01032f0718p11690ajsn743f8a8b1aa5',
+    'X-RapidAPI-Key': 'key',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
